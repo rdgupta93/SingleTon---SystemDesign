6dbc5511319505e3f10b81a0d1630c405b85c11d
rdgupta93 <rdgupta1336@gmail.com>
2024-01-21 19:27:22 +0530

    Git push

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+     Singleton s1 = Singleton.getObject();
+        System.out.println(s1);
+        Singleton s2 = Singleton.getObject();
+        System.out.println(s2);
+        Singleton s3 = Singleton.getObject();
+        System.out.println(s3);
+        Singleton s4 = Singleton.getObject();
+        System.out.println(s4);
+
+
+    }
+}
